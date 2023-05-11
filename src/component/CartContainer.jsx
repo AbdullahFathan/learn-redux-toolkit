caf1fc8629e0fd4bb69e5c6294f7ac86c1b0c5fb
AbdullahFathan <abdullahfathan013@gmail.com>
2023-05-11 21:24:22 +0700

    feat: cart item menu

--- src/component/CartContainer.jsx
+++ src/component/CartContainer.jsx
@@ -1,7 +1,46 @@
+import { clearCart } from "../feature/cart/cart";
 import CartItem from "./CartItem";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartContainer = () => {
-  return <div>CartContainer</div>;
+  const dispatch = useDispatch();
+  const { cartItem, total, amount } = useSelector((state) => state.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is Curently Empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your Bag</h2>
+      </header>
+      <div>
+        {cartItem.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          {" "}
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
 };
 
 export default CartContainer;
--- src/feature/cart/cart.jsx
+++ src/feature/cart/cart.jsx
@@ -3,7 +3,7 @@ import CartItems from "../../cartItems";
 
 const initialState = {
   cartItem: CartItems,
-  amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
@@ -11,6 +11,25 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItem = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItem = state.cartItem.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItem.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItem.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
 });
+
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
