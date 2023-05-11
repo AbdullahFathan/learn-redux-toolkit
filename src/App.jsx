bc040f45a75577e354010be33fed53898d52f681
AbdullahFathan <abdullahfathan013@gmail.com>
2023-05-11 21:55:31 +0700

    feat: add function to button +/- in cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./component/CartContainer";
 import Navbar from "./component/Navbar";
+import { calculateTotals } from "./feature/cart/cart";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItem } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItem]);
   return (
     <main>
       <Navbar />
